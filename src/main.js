41176350275bffe8c7a92ac62a533e66a3e65274
elidianaandrade <lide_andrade_redes@hotmail.com>
2022-09-15 19:09:46 -0300

    pokemons list

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ const offset = 0;
 const limit = 9;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -24,14 +23,6 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonsList = document.getElementById('pokemonsList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonsList.innerHTML += convertPokemonToLi(pokemon)
-        }
-
-    })
-    .catch((error) => console.log(error))    
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonsList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
