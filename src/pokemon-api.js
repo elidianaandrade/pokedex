abb1cd6d7071ece76e84223d8031967b4f69023e
elidianaandrade <lide_andrade_redes@hotmail.com>
2022-09-17 19:43:42 -0300

    choose pokemon

--- src/pokemon-api.js
+++ src/pokemon-api.js
@@ -32,3 +32,49 @@ pokeApi.getPokemons = (offset = 0, limit = 9) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+/* CHOOSE POKEMON */
+const pokemonName = document.querySelector('.choose-pokemon__name');
+const pokemonNumber = document.querySelector('.choose-pokemon__number');
+const pokemonImg = document.querySelector('.choose-pokemon__img');
+
+const form = document.querySelector('.choose-pokemon__form');
+const input = document.querySelector('.choose-pokemon__search');
+let searchPokemon = 1;
+
+const getPokemon = async (pokemon) => {
+    const urlPokemon = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+    if(urlPokemon.status === 200) {
+        const pokemonData = await urlPokemon.json();
+        return pokemonData;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const pokemonData = await getPokemon(pokemon)
+
+    if(pokemonData) {
+        pokemonName.innerHTML = pokemonData.name
+        pokemonNumber.innerHTML = `nº ${pokemonData.id}`
+        pokemonImg.src = pokemonData.sprites.other.dream_world.front_default
+        input.value = '';
+    } else {
+        pokemonName.innerHTML = 'Pokémon not found'
+        pokemonNumber.innerHTML = ''
+        pokemonImg.src = 'src/assets/images/whos-that-pokemon.png'
+        input.value = '';
+    }
+
+
+}
+
+form.addEventListener('submit', (search) => {
+    search.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+  });
+
+renderPokemon(searchPokemon)
